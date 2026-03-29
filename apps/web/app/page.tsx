1dc5da99532ad761b2bd28003526405decffc725
Manav Singh <manavchaudhary060705@gmail.com>
2026-03-29 21:40:00 +0530

    MOVE TO DOCKER HUB CHANGE THE DB ENV FROM DEVELOPEMRNT TO PRODUCTION

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,8 +1,10 @@
+export const dynamic = "force-dynamic";
+
 import { prismaClient } from "@repo/db/client";
-export const revalidate = 5;
 
 export default async function Home() {
   const users = await prismaClient.user.findMany();
+
   return (
     <div>
       {JSON.stringify(users)}
