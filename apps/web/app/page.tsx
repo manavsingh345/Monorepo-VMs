0de2906e5e5d889cfc1972e680474a399763e2c1
Manav Singh <manavchaudhary060705@gmail.com>
2026-03-29 21:26:48 +0530

    fix: force dynamic rendering to prevent db access during build

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,4 +1,5 @@
 import { prismaClient } from "@repo/db/client";
+export const revalidate = 5;
 
 export default async function Home() {
   const users = await prismaClient.user.findMany();
@@ -8,4 +9,3 @@ export default async function Home() {
     </div>
   );
 }
-export const revalidate = 5;
